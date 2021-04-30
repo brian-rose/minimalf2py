e1b10c79b3067cd89b39c766fb1cc628835df0eb
Brian Rose <brose@albany.edu>
2021-04-30 14:25:11 -0400

    simplify conda recipe

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,17 +5,4 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 
-# Depending on our platform, shared libraries end with either .so or .dylib
-if [[ `uname` == 'Darwin' ]]; then
-    # Also, included a workaround so that `-stdlib=c++` doesn't go to
-    # `gfortran` and cause problems.
-    #
-    # https://github.com/conda-forge/toolchain-feedstock/pull/8
-    #export CFLAGS="${CFLAGS} -stdlib=libc++ -lc++"
-    #export LDFLAGS="-headerpad_max_install_names -undefined dynamic_lookup -bundle -Wl,-search_paths_first -lc++"
-    unset LDFLAGS
-else
-    unset LDFLAGS
-fi
-
 $PYTHON -m pip install . --no-deps -vv
